df1f5db85c1c9983315e33534ab58e5f9ca2a89b
Cypher <oladotunlaniyan@gmail.com>
2024-08-03 13:09:51 +0100

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,69 +1,3 @@
-export const data = [
-    {
-        title: 'Threads',
-        description: 'Discover, connect, and engage with a vibrant social media web app. Seamlessly connect with friends and create or join communities tailored to specific topics, fostering meaningful interactions and shared interests. Your digital space for connection and community awaits!',
-        projectLink: 'https://threads-yusufsdesigns.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/Threads',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/41a1d3314ca0df557e0fc3594783999c502a23d2-2476x1689.png',
-        tags: ['NEXT.JS', 'React JS', 'All'],
-        stacks: ['NEXT.JS', 'React JS', 'TypeScript', 'Clerk', 'Shadcn', 'uploadthing', 'MongoDB', 'Mongoose', 'Git'],
-    },
-    {
-        title: 'Flair',
-        description: 'An e-commerce web app that allows customers to add products to their cart, search for specific products, filter the products based on the category of their choice.',
-        projectLink: 'https://flair-ecommerce.web.app/',
-        codeLink: 'https://github.com/YusufsDesigns/Flair',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/0b11e3000fd7c28d7e047e8b6b54e9c0127bd64b-2546x1688.png',
-        tags: ['Web App', 'React JS', 'All'],
-        stacks: ['React JS', 'TypeScript', 'SCSS', 'Commerce.js', 'Context API', 'Firebase', 'Git'],
-    },
-    {
-        title: 'sociaLite',
-        description: 'A dynamic social media web application designed to connect users, foster engagement, with features such as posts, messages, channels and much more.',
-        projectLink: 'https://socialite-yusufsdesigns.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/sociaLite',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/41eee1cdf1e88db936d25afcc1212d2e6988e9aa-3008x1534.png',
-        tags: ['Web App', 'React JS', 'All'],
-        stacks: ['React JS', 'Tailwind CSS', 'Context API', 'Firebase', 'Git', 'Vercel'],
-    },
-    {
-        title: 'WinAmp',
-        description: 'A music player web application that allows users to stream audio and search for their favorite song or artist.',
-        projectLink: 'https://winamp-music-app.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/WinAmp-Music-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/1367265645508aed6320cb2b402d1580dcd0cb49-3008x1692.png',
-        tags: ['Web App', 'React JS', 'All'],
-        stacks: ['React JS', 'Tailwind CSS', 'Deezer API', 'Git', 'Vercel']
-    },
-    {
-        title: 'Chat App',
-        description: 'A chat application that allows users to connect and chat effortlessly with friends, and family, share messages, photos, and stay connected wherever you go.',
-        projectLink: 'https://chat-app-yusufsdesigns.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/Chat-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/d53f0b5a40c773f7d82c6c9ff06ae2f444b9e53e-2766x1692.png',
-        tags: ['Web App', 'React JS', 'All'],
-        stacks: ['React JS', 'SCSS', 'Firebase', 'Git', 'Vercel']
-    },
-    {
-        title: 'Home Rentals Landing Page',
-        description: 'Created a responsive home rentals landing page that has pagination and form validation',
-        projectLink: 'https://home-rentals-landing-page.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/Home-Rentals-Landing-Page',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/63ec9cb5e04750e743d4365eff18fc6f50057547-3008x1576.png',
-        tags: ['React JS', 'All'],
-        stacks: ['React JS', 'CSS', 'React-Paginate', 'Git', 'Vercel']
-    },
-    {
-        title: 'The Weather',
-        description: 'Built a weather application that displays the weather conditions of over 200,000 cities with a change of background depending on the weather condition and time of day of the particular city.',
-        projectLink: 'https://weather-app-yusufsdesigns.vercel.app/',
-        codeLink: 'https://github.com/YusufsDesigns/Weather-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/825e78329cd9b9486f2ed2d7eabad4caf3fa181c-3008x1692.png',
-        tags: ['Web App', 'All'],
-        stacks: ['HTML', 'CSS', 'JavaScript', 'WeatherAPI', 'Git', 'Vercel']
-    }
-]
-
 export const about = [
     {
         imgUrl: 'src/assets/about01.png',
@@ -95,7 +29,7 @@ export const works = [
       imgUrl: "src/assets/project1.png",
       projectLink: "https://greenland-five.vercel.app/",
       codeLink: "https://github.com/Oladotunlaniyan/greenland",
-      title: "Project 1",
+      title: "Green Land",
       description: "Greenland Five is a dynamic and innovative platform designed to foster collaboration and innovation in the tech industry. Hosted on Vercel, the site provides users with a seamless experience, leveraging cutting-edge technology to deliver top-notch performance and reliability. Greenland Five is dedicated to creating an environment where tech enthusiasts, developers, and innovators can connect, share ideas, and work together on groundbreaking projects. With a user-friendly interface and a robust set of features, it stands as a hub for creativity and technological advancement, driving the future of tech forward.",
       stacks: ["HTML", "CSS"]
     },
@@ -104,16 +38,16 @@ export const works = [
       imgUrl: "src/assets/project2.png",
       projectLink:"https://film-pire-clone.vercel.app/",
       codeLink: "https://github.com/Oladotunlaniyan/Film-pire-clone-2",
-      title: "Project 2",
+      title: "Filmpire Clone",
       description: "FilmPire Clone is an engaging and user-friendly web application that replicates the experience of a premier movie database platform. Hosted on Vercel, this site offers a sleek and intuitive interface where users can explore a vast collection of films, access detailed information about movies, actors, and directors, and stay updated with the latest in the world of cinema. Whether you're a casual moviegoer or a dedicated film buff, FilmPire Clone provides an immersive and comprehensive resource for all things film, making it easy to discover new favorites and revisit classic hits.",
       stacks: ["React JS", "Redux"]
     },
     {
-        name: "9SAP ecommerce store",
+        name: "95AP ecommerce store",
         imgUrl: "src/assets/project3.png",
         projectLink: "https://ninefiveap.com/",
         codeLink: "https://github.com/Oladotunlaniyan",
-        title: "Project 3",
+        title: "95AP ecommerce store",
         description: "Nine Five AP is a sophisticated and streamlined platform built with Shopify, designed to provide a comprehensive and efficient solution for professional needs. Hosted on Vercel, the site combines the robust e-commerce capabilities of Shopify with exceptional performance and user experience. Nine Five AP integrates cutting-edge technology to ensure reliability and speed, offering a range of features tailored to enhance productivity, streamline workflows, and facilitate seamless collaboration. Ideal for businesses and professionals seeking to optimize their operations, Nine Five AP stands as a versatile and robust tool in the modern digital landscape.",
         stacks: ["Shopify", "CMS"]
       },
@@ -122,7 +56,7 @@ export const works = [
         imgUrl: "src/assets/project4.png",
         projectLink: "https://youtube-clone-ten-alpha.vercel.app/",
         codeLink: "https://github.com/Oladotunlaniyan/Youtube-Clone",
-        title: "Project 4",
+        title: "Youtube Clone",
         description: "YouTube Clone is an innovative and user-friendly web application hosted on Vercel, designed to replicate the experience of the popular video-sharing platform, YouTube. This clone offers a sleek and intuitive interface where users can browse, upload, and watch a wide variety of videos. With features that mimic the original platform, such as video recommendations, search functionality, and user profiles, YouTube Clone provides an immersive experience for both content creators and viewers. Whether you're looking to share your videos with the world or discover new content, YouTube Clone offers a seamless and engaging platform to fulfill your video-sharing needs.",
         stacks: ["HTML", "CSS"]
       },
@@ -131,12 +65,10 @@ export const works = [
         imgUrl: "src/assets/project5.png",
         projectLink: "https://react-portfolio-1-chi.vercel.app/",
         codeLink: "https://github.com/Oladotunlaniyan/React-portfolio-1",
-        title: "Project 5",
+        title: "React Portofolio",
         description: "React Portfolio is a meticulously crafted dummy portfolio hosted on Vercel, designed to showcase the potential and creativity of web development using React. This portfolio features a clean, modern design with an intuitive layout, providing an excellent example of how to present professional skills, projects, and experiences. Whether you are a developer looking to create your own portfolio or simply exploring the capabilities of React, this dummy portfolio serves as a comprehensive template, demonstrating best practices in design and functionality. With smooth navigation and engaging visuals, React Portfolio stands as a testament to the power and versatility of modern web development technologies.",
         stacks: ["React JS", "Redux"]
       },
-     
-    // Add more projects as needed
   ];
   
 // src/Data.js
@@ -144,72 +76,71 @@ export const skills = [
     {
       name: "JavaScript",
       bgColor: "#e4e4e4",
-      icon: "src/assets/javascript.png", // Ensure this path is correct
+      icon: "src/assets/javascript.png", 
     },
     {
       name: "React",
       bgColor: "#e4e4e4",
-      icon: "src/assets/react.png", // Ensure this path is correct
+      icon: "src/assets/react.png", 
     },
     {
       name: "CSS",
       bgColor: "#e4e4e4",
-      icon: "src/assets/css.png", // Ensure this path is correct
+      icon: "src/assets/css.png", 
     },
     {
         name: "tailwind",
         bgColor: "#e4e4e4",
-        icon: "src/assets/tailwind.png", // Ensure this path is correct
+        icon: "src/assets/tailwind.png", 
       },
       {
         name: "vue",
         bgColor: "#e4e4e4",
-        icon: "src/assets/vue.png", // Ensure this path is correct
+        icon: "src/assets/vue.png", 
       },
       {
         name: "node",
         bgColor: "#e4e4e4",
-        icon: "src/assets/node.png", // Ensure this path is correct
+        icon: "src/assets/node.png", 
       },
       {
         name: "Nextjs",
         bgColor: "#e4e4e4",
-        icon: "src/assets/nextjs.png", // Ensure this path is correct
+        icon: "src/assets/nextjs.png", 
       },
       {
         name: "GraphQl",
         bgColor: "#e4e4e4",
-        icon: "src/assets/graphql.png", // Ensure this path is correct
+        icon: "src/assets/graphql.png", 
       },
       {
         name: "Git",
         bgColor: "#e4e4e4",
-        icon: "src/assets/git.png", // Ensure this path is correct
+        icon: "src/assets/git.png", 
       },
       {
         name: "Figma",
         bgColor: "#e4e4e4",
-        icon: "src/assets/figma.png", // Ensure this path is correct
+        icon: "src/assets/figma.png", 
       },
       {
         name: "Firebase",
         bgColor: "#e4e4e4",
-        icon: "src/assets/firebase.png", // Ensure this path is correct
+        icon: "src/assets/firebase.png", 
       },
       {
         name: "Redux",
         bgColor: "#e4e4e4",
-        icon: "src/assets/redux.png", // Ensure this path is correct
+        icon: "src/assets/redux.png", 
       },
       {
         name: "SASS",
         bgColor: "#e4e4e4",
-        icon: "src/assets/sass.png", // Ensure this path is correct
+        icon: "src/assets/sass.png", 
       },
       {
         name: "Typescript",
         bgColor: "#e4e4e4",
-        icon: "src/assets/typescript.png", // Ensure this path is correct
+        icon: "src/assets/typescript.png", 
       },
-    // Add more skills as needed
   ];

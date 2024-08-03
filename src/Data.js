06d231287cfd90ac1890df2f4232e7cc7a66e5c8
Cypher <oladotunlaniyan@gmail.com>
2024-08-03 14:16:20 +0100

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,146 +1,143 @@
 export const about = [
     {
-        imgUrl: 'src/assets/about01.png',
+        imgUrl: '/assets/about01.png',
         title: 'Frontend Developer',
         description: 'A Frontend Developer with expertise in crafting engaging user experiences.',
     },
     {
-        imgUrl: 'src/assets/about02.png',
+        imgUrl: '/assets/about02.png',
         title: 'Freelancer',
         description: 'An experienced freelance frontend developer capable of bringing a clients dream site to reality.',
     },
     {
-        imgUrl: 'src/assets/about03.png',
+        imgUrl: '/assets/about03.png',
         title: 'Technical Writer',
         description: 'A writer with experience in crafting user-centric technical documentations',
     },
     {
-        imgUrl: 'src/assets/about04.png',
+        imgUrl: '/assets/about04.png',
         title: 'React Developer',
         description: 'A React Developer with a passion for building dynamic and interactive user interfaces.',
     },
-]
+];
 
-// src/data.js
-// src/data.js
 export const works = [
     {
-      name: "Green Land",
-      imgUrl: "src/assets/project1.png",
-      projectLink: "https://greenland-five.vercel.app/",
-      codeLink: "https://github.com/Oladotunlaniyan/greenland",
-      title: "Green Land",
-      description: "Greenland Five is a dynamic and innovative platform designed to foster collaboration and innovation in the tech industry. Hosted on Vercel, the site provides users with a seamless experience, leveraging cutting-edge technology to deliver top-notch performance and reliability. Greenland Five is dedicated to creating an environment where tech enthusiasts, developers, and innovators can connect, share ideas, and work together on groundbreaking projects. With a user-friendly interface and a robust set of features, it stands as a hub for creativity and technological advancement, driving the future of tech forward.",
-      stacks: ["HTML", "CSS"]
+        name: "Green Land",
+        imgUrl: "/assets/project1.png",
+        projectLink: "https://greenland-five.vercel.app/",
+        codeLink: "https://github.com/Oladotunlaniyan/greenland",
+        title: "Green Land",
+        description: "Greenland Five is a dynamic and innovative platform designed to foster collaboration and innovation in the tech industry. Hosted on Vercel, the site provides users with a seamless experience, leveraging cutting-edge technology to deliver top-notch performance and reliability. Greenland Five is dedicated to creating an environment where tech enthusiasts, developers, and innovators can connect, share ideas, and work together on groundbreaking projects. With a user-friendly interface and a robust set of features, it stands as a hub for creativity and technological advancement, driving the future of tech forward.",
+        stacks: ["HTML", "CSS"]
     },
     {
-      name: "Filmpire Clone",
-      imgUrl: "src/assets/project2.png",
-      projectLink:"https://film-pire-clone.vercel.app/",
-      codeLink: "https://github.com/Oladotunlaniyan/Film-pire-clone-2",
-      title: "Filmpire Clone",
-      description: "FilmPire Clone is an engaging and user-friendly web application that replicates the experience of a premier movie database platform. Hosted on Vercel, this site offers a sleek and intuitive interface where users can explore a vast collection of films, access detailed information about movies, actors, and directors, and stay updated with the latest in the world of cinema. Whether you're a casual moviegoer or a dedicated film buff, FilmPire Clone provides an immersive and comprehensive resource for all things film, making it easy to discover new favorites and revisit classic hits.",
-      stacks: ["React JS", "Redux"]
+        name: "Filmpire Clone",
+        imgUrl: "/assets/project2.png",
+        projectLink: "https://film-pire-clone.vercel.app/",
+        codeLink: "https://github.com/Oladotunlaniyan/Film-pire-clone-2",
+        title: "Filmpire Clone",
+        description: "FilmPire Clone is an engaging and user-friendly web application that replicates the experience of a premier movie database platform. Hosted on Vercel, this site offers a sleek and intuitive interface where users can explore a vast collection of films, access detailed information about movies, actors, and directors, and stay updated with the latest in the world of cinema. Whether you're a casual moviegoer or a dedicated film buff, FilmPire Clone provides an immersive and comprehensive resource for all things film, making it easy to discover new favorites and revisit classic hits.",
+        stacks: ["React JS", "Redux"]
     },
     {
         name: "95AP ecommerce store",
-        imgUrl: "src/assets/project3.png",
+        imgUrl: "/assets/project3.png",
         projectLink: "https://ninefiveap.com/",
         codeLink: "https://github.com/Oladotunlaniyan",
         title: "95AP ecommerce store",
         description: "Nine Five AP is a sophisticated and streamlined platform built with Shopify, designed to provide a comprehensive and efficient solution for professional needs. Hosted on Vercel, the site combines the robust e-commerce capabilities of Shopify with exceptional performance and user experience. Nine Five AP integrates cutting-edge technology to ensure reliability and speed, offering a range of features tailored to enhance productivity, streamline workflows, and facilitate seamless collaboration. Ideal for businesses and professionals seeking to optimize their operations, Nine Five AP stands as a versatile and robust tool in the modern digital landscape.",
         stacks: ["Shopify", "CMS"]
-      },
+    },
     {
         name: "Youtube Clone",
-        imgUrl: "src/assets/project4.png",
+        imgUrl: "/assets/project4.png",
         projectLink: "https://youtube-clone-ten-alpha.vercel.app/",
         codeLink: "https://github.com/Oladotunlaniyan/Youtube-Clone",
         title: "Youtube Clone",
         description: "YouTube Clone is an innovative and user-friendly web application hosted on Vercel, designed to replicate the experience of the popular video-sharing platform, YouTube. This clone offers a sleek and intuitive interface where users can browse, upload, and watch a wide variety of videos. With features that mimic the original platform, such as video recommendations, search functionality, and user profiles, YouTube Clone provides an immersive experience for both content creators and viewers. Whether you're looking to share your videos with the world or discover new content, YouTube Clone offers a seamless and engaging platform to fulfill your video-sharing needs.",
         stacks: ["HTML", "CSS"]
-      },
-      {
+    },
+    {
         name: "React Portofolio",
-        imgUrl: "src/assets/project5.png",
+        imgUrl: "/assets/project5.png",
         projectLink: "https://react-portfolio-1-chi.vercel.app/",
         codeLink: "https://github.com/Oladotunlaniyan/React-portfolio-1",
         title: "React Portofolio",
         description: "React Portfolio is a meticulously crafted dummy portfolio hosted on Vercel, designed to showcase the potential and creativity of web development using React. This portfolio features a clean, modern design with an intuitive layout, providing an excellent example of how to present professional skills, projects, and experiences. Whether you are a developer looking to create your own portfolio or simply exploring the capabilities of React, this dummy portfolio serves as a comprehensive template, demonstrating best practices in design and functionality. With smooth navigation and engaging visuals, React Portfolio stands as a testament to the power and versatility of modern web development technologies.",
         stacks: ["React JS", "Redux"]
-      },
-  ];
-  
-// src/Data.js
+    },
+];
+
 export const skills = [
     {
-      name: "JavaScript",
-      bgColor: "#e4e4e4",
-      icon: "src/assets/javascript.png", 
+        name: "JavaScript",
+        bgColor: "#e4e4e4",
+        icon: "/assets/javascript.png", 
     },
     {
-      name: "React",
-      bgColor: "#e4e4e4",
-      icon: "src/assets/react.png", 
+        name: "React",
+        bgColor: "#e4e4e4",
+        icon: "/assets/react.png", 
     },
     {
-      name: "CSS",
-      bgColor: "#e4e4e4",
-      icon: "src/assets/css.png", 
+        name: "CSS",
+        bgColor: "#e4e4e4",
+        icon: "/assets/css.png", 
     },
     {
         name: "tailwind",
         bgColor: "#e4e4e4",
-        icon: "src/assets/tailwind.png", 
-      },
-      {
+        icon: "/assets/tailwind.png", 
+    },
+    {
         name: "vue",
         bgColor: "#e4e4e4",
-        icon: "src/assets/vue.png", 
-      },
-      {
+        icon: "/assets/vue.png", 
+    },
+    {
         name: "node",
         bgColor: "#e4e4e4",
-        icon: "src/assets/node.png", 
-      },
-      {
+        icon: "/assets/node.png", 
+    },
+    {
         name: "Nextjs",
         bgColor: "#e4e4e4",
-        icon: "src/assets/nextjs.png", 
-      },
-      {
+        icon: "/assets/nextjs.png", 
+    },
+    {
         name: "GraphQl",
         bgColor: "#e4e4e4",
-        icon: "src/assets/graphql.png", 
-      },
-      {
+        icon: "/assets/graphql.png", 
+    },
+    {
         name: "Git",
         bgColor: "#e4e4e4",
-        icon: "src/assets/git.png", 
-      },
-      {
+        icon: "/assets/git.png", 
+    },
+    {
         name: "Figma",
         bgColor: "#e4e4e4",
-        icon: "src/assets/figma.png", 
-      },
-      {
+        icon: "/assets/figma.png", 
+    },
+    {
         name: "Firebase",
         bgColor: "#e4e4e4",
-        icon: "src/assets/firebase.png", 
-      },
-      {
+        icon: "/assets/firebase.png", 
+    },
+    {
         name: "Redux",
         bgColor: "#e4e4e4",
-        icon: "src/assets/redux.png", 
-      },
-      {
+        icon: "/assets/redux.png", 
+    },
+    {
         name: "SASS",
         bgColor: "#e4e4e4",
-        icon: "src/assets/sass.png", 
-      },
-      {
+        icon: "/assets/sass.png", 
+    },
+    {
         name: "Typescript",
         bgColor: "#e4e4e4",
-        icon: "src/assets/typescript.png", 
-      },
-  ];
+        icon: "/assets/typescript.png", 
+    },
+];

9d76eeb6f1123592995445ffd95a352c8b31c9f1
Cypher <oladotunlaniyan@gmail.com>
2024-08-17 14:21:22 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,65 +6,82 @@ import { MdEmail } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 
+const links = ['home', 'about', 'work', 'skills', 'contact'];
+
+const SocialLinks = () => (
+  <div className='socials'>
+    <a href='https://x.com/Devdotun' target='_blank' rel='noreferrer' aria-label='Twitter'>
+      <BsTwitter />
+    </a>
+    <a href='https://www.linkedin.com/in/abdul-qawi-laniyan-173355248/' target='_blank' rel='noreferrer' aria-label='LinkedIn'>
+      <BsLinkedin />
+    </a>
+    <a href='https://github.com/Oladotunlaniyan' target='_blank' rel='noreferrer' aria-label='GitHub'>
+      <BsGithub />
+    </a>
+    <a href='mailto: oladotunlaniyan@gmail.com' aria-label='Email'>
+      <MdEmail />
+    </a>
+  </div>
+);
+
+const NavbarLinks = ({ onClick }) => (
+  <ul className='app__navbar-links'>
+    {links.map(item => (
+      <li className='app__flex p-text' key={item}>
+        <div></div>
+        <a href={`#${item}`} onClick={onClick}>{item}</a>
+      </li>
+    ))}
+    <li>
+      <a href={images.resume} rel='noreferrer' target='_blank'>My Resume</a>
+    </li>
+  </ul>
+);
+
 const Navbar = () => {
-  const [animate, setAnimate] = useState({ x: 300, opacity: 0 });
-  const links = ['home', 'about', 'work', 'skills', 'contact'];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        {/* <img src={images.logo} alt="" /> */}
         <b><i>Abdul-Qawi</i></b>
       </div>
-      <ul className='app__navbar-links'>
-        {links.map(item => (
-          <li className='app__flex p-text' key={`link-${item}`}>
-            <div></div>
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
 
-      <div className="app__navbar-menu" onClick={() => setAnimate({ x: 0, opacity: 1 })}>
-        <HiMenuAlt4 />
+      <div className='app__navbar-links-container'>
+        <NavbarLinks />
       </div>
-      <motion.div
-        initial={{ x: 300, opacity: 0 }}
-        animate={animate}
-        transition={{ duration: 0.85, ease: 'easeInOut' }}
-        className='menu'
+
+      <button
+        className='app__navbar-menu'
+        onClick={handleMenuToggle}
+        aria-label='Toggle menu'
+        aria-expanded={isMenuOpen}
       >
-        <HiX onClick={() => setAnimate({ x: 300, opacity: 0 })} />
-        <ul>
-          {links.map(item => (
-            <li key={item}>
-              <a href={`#${item}`} onClick={() => setAnimate({ x: 300, opacity: 0 })}>{item}</a>
-            </li>
-          ))}
-          <li>
-            <a href={images.resume} rel="noreferrer" target="_blank">My Resume</a>
-          </li>
+        <HiMenuAlt4 />
+      </button>
+
+      {isMenuOpen && (
+        <motion.div
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.85, ease: 'easeInOut' }}
+          className='menu'
+        >
+          <HiX onClick={handleMenuToggle} aria-label='Close menu' />
+          <NavbarLinks onClick={handleMenuToggle} />
           <div className='app__flex'>
-            <h2>Let&lsquo;s Collaborate</h2>
-            <div className='socials'>
-              <a href='https://x.com/Devdotun' target='blank'>
-                <BsTwitter />
-              </a>
-              <a href='https://www.linkedin.com/in/abdul-qawi-laniyan-173355248/' target='blank'>
-                <BsLinkedin />
-              </a>
-              <a href='https://github.com/Oladotunlaniyan' target='blank'>
-                <BsGithub />
-              </a>
-              <a href="mailto: oladotunlaniyan@gmail.com">
-                <MdEmail />
-              </a>
-            </div>
+            <h2>Let’s Collaborate</h2>
+            <SocialLinks />
           </div>
-        </ul>
-      </motion.div>
+        </motion.div>
+      )}
     </nav>
   );
-}
+};
 
 export default Navbar;

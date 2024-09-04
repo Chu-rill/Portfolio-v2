1726a11510676f23a9857e90d59023c6fe5bab11
Chu-rill <churchillrimamskep@gmail.com>
2024-09-04 20:38:37 +0100

    changed the social media links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,31 +1,39 @@
-import { BsTwitter, BsLinkedin, BsGithub, BsFileEarmarkPdfFill } from 'react-icons/bs'
-import { MdEmail } from 'react-icons/md'
-import { images } from '../constants'
+import {
+  BsTwitter,
+  BsLinkedin,
+  BsGithub,
+  BsFileEarmarkPdfFill,
+} from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { images } from "../constants";
 import { SiHashnode } from "react-icons/si";
 
 const SocialMedia = () => {
-    return (
-        <div className='app__social'>
-            <a href='https://devdotun.hashnode.dev/' target='blank'>
+  return (
+    <div className="app__social">
+      {/* <a href='https://devdotun.hashnode.dev/' target='blank'>
             <SiHashnode />
-            </a>
-            <a href='https://x.com/Devdotun' target='blank'>
-                <BsTwitter />
-            </a>
-            <a href='https://www.linkedin.com/in/abdul-qawi-laniyan-173355248/' target='blank'>
-                <BsLinkedin />
-            </a>
-            <a href='https://github.com/Oladotunlaniyan' target='blank'>
-                <BsGithub />
-            </a>
-            <a href="mailto: oladotunlaniyan@gmail.com">
-                <MdEmail />
-            </a>
-            <a href={images.resume} target='_blank' rel="noreferrer">
-                <BsFileEarmarkPdfFill />
-            </a>
-        </div>
-    )
-}
+            </a> */}
+      <a href="https://x.com/chu_rill" target="blank">
+        <BsTwitter />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/churchill-daniel-b66a752a7/"
+        target="blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/Chu-rill" target="blank">
+        <BsGithub />
+      </a>
+      <a href="mailto: churchilldaniel687@gmail.com">
+        <MdEmail />
+      </a>
+      <a href={images.resume} target="_blank" rel="noreferrer">
+        <BsFileEarmarkPdfFill />
+      </a>
+    </div>
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;

c982828b089c88b17a010ab4a9e04f9ae3e55638
Chu-rill <churchillrimamskep@gmail.com>
2024-09-04 16:03:33 +0100

    changed the about section

--- src/Data.js
+++ src/Data.js
@@ -1,168 +1,171 @@
-import about01 from './assets/about01.png';
-import about02 from './assets/about02.png';
-import about03 from './assets/about03.png';
-import about04 from './assets/about04.png';
-import project1 from './assets/project1.png';
-import project2 from './assets/project2.png';
-import project3 from './assets/project3.png';
-import project4 from './assets/project4.png';
-import project5 from './assets/project5.png';
-import javascript from './assets/javascript.png';
-import react from './assets/react.png';
-import css from './assets/css.png';
-import tailwind from './assets/tailwind.png';
-import vue from './assets/vue.png';
-import node from './assets/node.png';
-import nextjs from './assets/nextjs.png';
-import graphql from './assets/graphql.png';
-import git from './assets/git.png';
-import figma from './assets/figma.png';
-import firebase from './assets/firebase.png';
-import redux from './assets/redux.png';
-import sass from './assets/sass.png';
-import typescript from './assets/typescript.png';
-
+import about01 from "./assets/about01.png";
+import about02 from "./assets/about02.png";
+import about03 from "./assets/about03.png";
+import about04 from "./assets/about04.png";
+import project1 from "./assets/project1.png";
+import project2 from "./assets/project2.png";
+import project3 from "./assets/project3.png";
+import project4 from "./assets/project4.png";
+import project5 from "./assets/project5.png";
+import javascript from "./assets/javascript.png";
+import react from "./assets/react.png";
+import css from "./assets/css.png";
+import tailwind from "./assets/tailwind.png";
+import vue from "./assets/vue.png";
+import node from "./assets/node.png";
+import nextjs from "./assets/nextjs.png";
+import graphql from "./assets/graphql.png";
+import git from "./assets/git.png";
+import figma from "./assets/figma.png";
+import firebase from "./assets/firebase.png";
+import redux from "./assets/redux.png";
+import sass from "./assets/sass.png";
+import typescript from "./assets/typescript.png";
 
 export const about = [
-    {
-        imgUrl: about01,
-        title: 'Frontend Developer',
-        description: 'A Frontend Developer with expertise in crafting engaging user experiences.',
-    },
-    {
-        imgUrl: about02,
-        title: 'Freelancer',
-        description: 'An experienced freelance frontend developer capable of bringing a clients dream site to reality.',
-    },
-    {
-        imgUrl: about03,
-        title: 'Technical Writer',
-        description: 'A writer with experience in crafting user-centric technical documentations',
-    },
-    {
-        imgUrl: about04,
-        title: 'React Developer',
-        description: 'A React Developer with a passion for building dynamic and interactive user interfaces.',
-    },
+  {
+    imgUrl: about01,
+    title: "Frontend Developer",
+    description:
+      "A Frontend Developer with expertise in crafting engaging user experiences.",
+  },
+  {
+    imgUrl: about02,
+    title: "Backend Developer",
+    description:
+      "A Backend Developer with expertise in building robust and scalable server-side applications.",
+  },
+
+  {
+    imgUrl: about04,
+    title: "React Developer",
+    description:
+      "A React Developer with a passion for building dynamic and interactive user interfaces.",
+  },
 ];
 
 export const works = [
-    {
-        name: "Green Land",
-        imgUrl: project1,
-        projectLink: "https://greenland-five.vercel.app/",
-        codeLink: "https://github.com/Oladotunlaniyan/greenland",
-        title: "Green Land",
-        description: "Greenland Five is a dynamic and innovative platform designed to foster collaboration and innovation in the tech industry. Hosted on Vercel, the site provides users with a seamless experience, leveraging cutting-edge technology to deliver top-notch performance and reliability. Greenland Five is dedicated to creating an environment where tech enthusiasts, developers, and innovators can connect, share ideas, and work together on groundbreaking projects. With a user-friendly interface and a robust set of features, it stands as a hub for creativity and technological advancement, driving the future of tech forward.",
-        stacks: ["HTML", "CSS"]
-    },
-    {
-        name: "Filmpire Clone",
-        imgUrl: project2,
-        projectLink: "https://film-pire-clone.vercel.app/",
-        codeLink: "https://github.com/Oladotunlaniyan/Film-pire-clone-2",
-        title: "Filmpire Clone",
-        description: "FilmPire Clone is an engaging and user-friendly web application that replicates the experience of a premier movie database platform. Hosted on Vercel, this site offers a sleek and intuitive interface where users can explore a vast collection of films, access detailed information about movies, actors, and directors, and stay updated with the latest in the world of cinema. Whether you're a casual moviegoer or a dedicated film buff, FilmPire Clone provides an immersive and comprehensive resource for all things film, making it easy to discover new favorites and revisit classic hits.",
-        stacks: ["React JS", "Redux"]
-    },
-    {
-        name: "95AP ecommerce store",
-        imgUrl: project3,
-        projectLink: "https://ninefiveap.com/",
-        codeLink: "https://github.com/Oladotunlaniyan",
-        title: "95AP ecommerce store",
-        description: "Nine Five AP is a sophisticated and streamlined platform built with Shopify, designed to provide a comprehensive and efficient solution for professional needs. Hosted on Vercel, the site combines the robust e-commerce capabilities of Shopify with exceptional performance and user experience. Nine Five AP integrates cutting-edge technology to ensure reliability and speed, offering a range of features tailored to enhance productivity, streamline workflows, and facilitate seamless collaboration. Ideal for businesses and professionals seeking to optimize their operations, Nine Five AP stands as a versatile and robust tool in the modern digital landscape.",
-        stacks: ["Shopify", "CMS"]
-    },
-    {
-        name: "Youtube Clone",
-        imgUrl: project4,
-        projectLink: "https://youtube-clone-ten-alpha.vercel.app/",
-        codeLink: "https://github.com/Oladotunlaniyan/Youtube-Clone",
-        title: "Youtube Clone",
-        description: "YouTube Clone is an innovative and user-friendly web application hosted on Vercel, designed to replicate the experience of the popular video-sharing platform, YouTube. This clone offers a sleek and intuitive interface where users can browse, upload, and watch a wide variety of videos. With features that mimic the original platform, such as video recommendations, search functionality, and user profiles, YouTube Clone provides an immersive experience for both content creators and viewers. Whether you're looking to share your videos with the world or discover new content, YouTube Clone offers a seamless and engaging platform to fulfill your video-sharing needs.",
-        stacks: ["HTML", "CSS"]
-    },
-    {
-        name: "React Portofolio",
-        imgUrl: project5,
-        projectLink: "https://react-portfolio-1-chi.vercel.app/",
-        codeLink: "https://github.com/Oladotunlaniyan/React-portfolio-1",
-        title: "React Portofolio",
-        description: "React Portfolio is a meticulously crafted dummy portfolio hosted on Vercel, designed to showcase the potential and creativity of web development using React. This portfolio features a clean, modern design with an intuitive layout, providing an excellent example of how to present professional skills, projects, and experiences. Whether you are a developer looking to create your own portfolio or simply exploring the capabilities of React, this dummy portfolio serves as a comprehensive template, demonstrating best practices in design and functionality. With smooth navigation and engaging visuals, React Portfolio stands as a testament to the power and versatility of modern web development technologies.",
-        stacks: ["React JS", "Redux"]
-    },
+  {
+    name: "Green Land",
+    imgUrl: project1,
+    projectLink: "https://greenland-five.vercel.app/",
+    codeLink: "https://github.com/Oladotunlaniyan/greenland",
+    title: "Green Land",
+    description:
+      "Greenland Five is a dynamic and innovative platform designed to foster collaboration and innovation in the tech industry. Hosted on Vercel, the site provides users with a seamless experience, leveraging cutting-edge technology to deliver top-notch performance and reliability. Greenland Five is dedicated to creating an environment where tech enthusiasts, developers, and innovators can connect, share ideas, and work together on groundbreaking projects. With a user-friendly interface and a robust set of features, it stands as a hub for creativity and technological advancement, driving the future of tech forward.",
+    stacks: ["HTML", "CSS"],
+  },
+  {
+    name: "Filmpire Clone",
+    imgUrl: project2,
+    projectLink: "https://film-pire-clone.vercel.app/",
+    codeLink: "https://github.com/Oladotunlaniyan/Film-pire-clone-2",
+    title: "Filmpire Clone",
+    description:
+      "FilmPire Clone is an engaging and user-friendly web application that replicates the experience of a premier movie database platform. Hosted on Vercel, this site offers a sleek and intuitive interface where users can explore a vast collection of films, access detailed information about movies, actors, and directors, and stay updated with the latest in the world of cinema. Whether you're a casual moviegoer or a dedicated film buff, FilmPire Clone provides an immersive and comprehensive resource for all things film, making it easy to discover new favorites and revisit classic hits.",
+    stacks: ["React JS", "Redux"],
+  },
+  {
+    name: "95AP ecommerce store",
+    imgUrl: project3,
+    projectLink: "https://ninefiveap.com/",
+    codeLink: "https://github.com/Oladotunlaniyan",
+    title: "95AP ecommerce store",
+    description:
+      "Nine Five AP is a sophisticated and streamlined platform built with Shopify, designed to provide a comprehensive and efficient solution for professional needs. Hosted on Vercel, the site combines the robust e-commerce capabilities of Shopify with exceptional performance and user experience. Nine Five AP integrates cutting-edge technology to ensure reliability and speed, offering a range of features tailored to enhance productivity, streamline workflows, and facilitate seamless collaboration. Ideal for businesses and professionals seeking to optimize their operations, Nine Five AP stands as a versatile and robust tool in the modern digital landscape.",
+    stacks: ["Shopify", "CMS"],
+  },
+  {
+    name: "Youtube Clone",
+    imgUrl: project4,
+    projectLink: "https://youtube-clone-ten-alpha.vercel.app/",
+    codeLink: "https://github.com/Oladotunlaniyan/Youtube-Clone",
+    title: "Youtube Clone",
+    description:
+      "YouTube Clone is an innovative and user-friendly web application hosted on Vercel, designed to replicate the experience of the popular video-sharing platform, YouTube. This clone offers a sleek and intuitive interface where users can browse, upload, and watch a wide variety of videos. With features that mimic the original platform, such as video recommendations, search functionality, and user profiles, YouTube Clone provides an immersive experience for both content creators and viewers. Whether you're looking to share your videos with the world or discover new content, YouTube Clone offers a seamless and engaging platform to fulfill your video-sharing needs.",
+    stacks: ["HTML", "CSS"],
+  },
+  {
+    name: "React Portofolio",
+    imgUrl: project5,
+    projectLink: "https://react-portfolio-1-chi.vercel.app/",
+    codeLink: "https://github.com/Oladotunlaniyan/React-portfolio-1",
+    title: "React Portofolio",
+    description:
+      "React Portfolio is a meticulously crafted dummy portfolio hosted on Vercel, designed to showcase the potential and creativity of web development using React. This portfolio features a clean, modern design with an intuitive layout, providing an excellent example of how to present professional skills, projects, and experiences. Whether you are a developer looking to create your own portfolio or simply exploring the capabilities of React, this dummy portfolio serves as a comprehensive template, demonstrating best practices in design and functionality. With smooth navigation and engaging visuals, React Portfolio stands as a testament to the power and versatility of modern web development technologies.",
+    stacks: ["React JS", "Redux"],
+  },
 ];
 
 export const skills = [
-    {
-        name: "JavaScript",
-        bgColor: "#e4e4e4",
-        icon: javascript,
-    },
-    {
-        name: "React",
-        bgColor: "#e4e4e4",
-        icon: react,
-    },
-    {
-        name: "CSS",
-        bgColor: "#e4e4e4",
-        icon: css,
-    },
-    {
-        name: "tailwind",
-        bgColor: "#e4e4e4",
-        icon: tailwind,
-    },
-    {
-        name: "vue",
-        bgColor: "#e4e4e4",
-        icon: vue,
-    },
-    {
-        name: "node",
-        bgColor: "#e4e4e4",
-        icon: node,
-    },
-    {
-        name: "Nextjs",
-        bgColor: "#e4e4e4",
-        icon: nextjs,
-    },
-    {
-        name: "GraphQl",
-        bgColor: "#e4e4e4",
-        icon: graphql,
-    },
-    {
-        name: "Git",
-        bgColor: "#e4e4e4",
-        icon: git,
-    },
-    {
-        name: "Figma",
-        bgColor: "#e4e4e4",
-        icon: figma,
-    },
-    {
-        name: "Firebase",
-        bgColor: "#e4e4e4",
-        icon: firebase,
-    },
-    {
-        name: "Redux",
-        bgColor: "#e4e4e4",
-        icon: redux,
-    },
-    {
-        name: "SASS",
-        bgColor: "#e4e4e4",
-        icon: sass,
-    },
-    {
-        name: "Typescript",
-        bgColor: "#e4e4e4",
-        icon: typescript,
-    },
+  {
+    name: "JavaScript",
+    bgColor: "#e4e4e4",
+    icon: javascript,
+  },
+  {
+    name: "React",
+    bgColor: "#e4e4e4",
+    icon: react,
+  },
+  {
+    name: "CSS",
+    bgColor: "#e4e4e4",
+    icon: css,
+  },
+  {
+    name: "tailwind",
+    bgColor: "#e4e4e4",
+    icon: tailwind,
+  },
+  {
+    name: "vue",
+    bgColor: "#e4e4e4",
+    icon: vue,
+  },
+  {
+    name: "node",
+    bgColor: "#e4e4e4",
+    icon: node,
+  },
+  {
+    name: "Nextjs",
+    bgColor: "#e4e4e4",
+    icon: nextjs,
+  },
+  {
+    name: "GraphQl",
+    bgColor: "#e4e4e4",
+    icon: graphql,
+  },
+  {
+    name: "Git",
+    bgColor: "#e4e4e4",
+    icon: git,
+  },
+  {
+    name: "Figma",
+    bgColor: "#e4e4e4",
+    icon: figma,
+  },
+  {
+    name: "Firebase",
+    bgColor: "#e4e4e4",
+    icon: firebase,
+  },
+  {
+    name: "Redux",
+    bgColor: "#e4e4e4",
+    icon: redux,
+  },
+  {
+    name: "SASS",
+    bgColor: "#e4e4e4",
+    icon: sass,
+  },
+  {
+    name: "Typescript",
+    bgColor: "#e4e4e4",
+    icon: typescript,
+  },
 ];
